f8a62c45ff7c3e4c995f06bc7c079c772a2d4c5d
Aviv Haruzi <avivharuzi@gmail.com>
2021-12-01 09:30:35 +0200

    feat(ddd): add schematics support

---
+++ packages/ddd/src/generators/init/schematic.ts
@@ -0,0 +1,5 @@
+import { convertNxGenerator } from '@nrwl/devkit';
+
+import dddInitGenerator from './generator';
+
+export default convertNxGenerator(dddInitGenerator);
---
+++ packages/ddd/src/generators/library/schematic.ts
@@ -0,0 +1,5 @@
+import { convertNxGenerator } from '@nrwl/devkit';
+
+import dddLibraryGenerator from './generator';
+
+export default convertNxGenerator(dddLibraryGenerator);
